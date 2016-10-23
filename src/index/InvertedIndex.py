44e39ea16abc84918b57623e439eac21c450c051
Alejandro <alejo8a_2@hotmail.com>
2016-10-23 10:33:08 -0500

    Subiendo algorimto de indice invertido

---
+++ src/index/InvertedIndex.py
@@ -0,0 +1,39 @@
+import os
+import re
+import unicodedata
+
+from mrjob.job import MRJob
+
+timesMap = {}
+
+def limpiar(line):
+    line = line.lower()
+    line = elimina_tildes(line)
+    line = re.findall(r"[\w']+", line)
+    return line
+
+def elimina_tildes(s):
+   return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
+
+class InvertedIndex(MRJob):
+
+    def mapper(self, _, line):
+        fileName = os.environ['map_input_file']
+        line = limpiar(line)
+
+        for word in line:
+            if (fileName, word) not in timesMap:
+                timesMap[(fileName, word)] = 1
+            else:
+                timesMap[(fileName, word)] +=1
+
+            yield (word, fileName)
+
+    def reducer(self, word, files):
+        lFiles = list(files)
+        fileTimes = [(x, timesMap[(x, word)]) for x in lFiles]
+        yield (word, len(lFiles)), list(set((fileTimes)))
+        # yield (word, len(lFiles)), list(set(lFiles))
+
+if __name__ == '__main__':
+    InvertedIndex.run()
